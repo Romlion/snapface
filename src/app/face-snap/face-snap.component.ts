b31e95b0c9a0766cc80a91172991c76bafc7a8d0
Romain <98345704+Romlion@users.noreply.github.com>
2025-05-12 21:16:54 +0200

    snap and unsnap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,16 +13,36 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = 'Captaine América';
     this.description = 'Le premier Avenger';
     this.createdAt = new Date();
     this.snaps = 0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png'
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png';
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
   }
 
-  onAddSnap(): void {
+  onSnap(): void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    } else {
+      this.snap();
+    }
+  }
+
+  unSnap() {
+    this.snaps--;
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
+  }
+
+  snap() {
     this.snaps++;
+    this.snapButtonText = 'Oops, unSnap!';
+    this.userHasSnapped = true;
   }
 }
