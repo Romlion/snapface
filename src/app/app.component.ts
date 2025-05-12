b82df721d3366ab73da47cc300feede486040cd6
Romain <98345704+Romlion@users.noreply.github.com>
2025-05-12 22:12:07 +0200

    contruire de component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,32 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Captaine América',
+      'Le premier Avenger',
+      'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png',
+      new Date(),
+      10
+    )
+    this.myOtherSnap = new FaceSnap(
+      'Three Rock Mountain',
+      'Un endroit magnifique pour les randonnées.',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      new Date(),
+      6,
+    )
+    this.myLastSnap = new FaceSnap(
+      'Un bon repas',
+      'Mmmh, que c\'est bon !',
+      'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      new Date(),
+      156
+    )
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,20 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+  @Input() faceSnap!: FaceSnap;
+
   snapButtonText!: string;
   userHasSnapped!: boolean;
 
   ngOnInit(): void {
-    this.title = 'Captaine América';
-    this.description = 'Le premier Avenger';
-    this.createdAt = new Date();
-    this.snaps = 0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png';
     this.snapButtonText = 'Oh Snap!';
     this.userHasSnapped = false;
   }
@@ -35,13 +28,13 @@ export class FaceSnapComponent implements OnInit {
   }
 
   unSnap() {
-    this.snaps--;
+    this.faceSnap.removeSnap();
     this.snapButtonText = 'Oh Snap!';
     this.userHasSnapped = false;
   }
 
   snap() {
-    this.snaps++;
+    this.faceSnap.addSnap();
     this.snapButtonText = 'Oops, unSnap!';
     this.userHasSnapped = true;
   }
