912e5f5737c4ac2825fbe64e72993771975a055d
Romain <98345704+Romlion@users.noreply.github.com>
2025-05-13 09:00:53 +0200

    affichez des listes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,37 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Captaine América',
-      'Le premier Avenger',
-      'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png',
-      new Date(),
-      10
-    )
-    this.myOtherSnap = new FaceSnap(
-      'Three Rock Mountain',
-      'Un endroit magnifique pour les randonnées.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      new Date(),
-      6,
-    )
-    this.myLastSnap = new FaceSnap(
-      'Un bon repas',
-      'Mmmh, que c\'est bon !',
-      'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      new Date(),
-      156
-    )
-
-    this.myOtherSnap.setLocation('a la montagne');
+    this.faceSnaps = [
+      new FaceSnap(
+        'Captaine América',
+        'Le premier Avenger',
+        'https://cdn.pixabay.com/photo/2023/06/29/08/22/ai-generated-8095915_960_720.png',
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6,
+      ),
+      new FaceSnap(
+        'Un bon repas',
+        'Mmmh, que c\'est bon !',
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      )
+    ]
+    this.faceSnaps[1].setLocation('a la montagne');
   }
 }
